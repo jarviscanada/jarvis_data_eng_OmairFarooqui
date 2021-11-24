79d425a24c6d17d28f64c40337c11272f8aae10e
Omair Farooqui <omairfarooqui25@gmail.com>
2021-11-24 19:58:35 +0000

    added monitoring agent scripts

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,7 +1,18 @@
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info
     (
-        id                  SERIAL NOT NULL PRIMARY KEY,
-        hostname            VARCHAR(255) NOT NULL UNIQUE,
+        "timestamp"           TIMESTAMP NOT NULL,
+        host_id               SERIAL PRIMARY KEY,
+        hostname              VARCHAR(255) NOT NULL UNIQUE,
+        memory_free           INTEGER NOT NULL,
+        cpu_idle              INTEGER NOT NULL,
+        cpu_kernel            INTEGER NOT NULL,
+        disk_io               INTEGER NOT NULL,
+        disk_available        INTEGER NOT NULL
+    );
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
+    (
+        id                  SERIAL NOT NULL REFERENCES host_info(host_id),
+        hostname            VARCHAR(255) NOT NULL,
         cpu_number          INTEGER NOT NULL,
         cpu_architecture    VARCHAR(255) NOT NULL,
         cpu_model           VARCHAR(255) NOT NULL,
@@ -11,13 +22,3 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
         "timestamp"         TIMESTAMP NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
-    (
-        "timestamp"           TIMESTAMP NOT NULL,
-        host_id               INTEGER NOT NULL REFERENCES host_info(id),
-        memory_free           INTEGER NOT NULL,
-        cpu_idle              INTEGER NOT NULL,
-        cpu_kernel            INTEGER NOT NULL,
-        disk_io               INTEGER NOT NULL,
-        disk_available        INTEGER NOT NULL
-    );
